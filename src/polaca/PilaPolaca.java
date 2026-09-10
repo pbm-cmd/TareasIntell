8dc2d16565a421acb6f45e4e76a60006d8fa2ab4
SSB-SYS <pbmgamer0@gmail.com>
2026-09-09 23:15:22 -0600

    Añadido la clase PilaPolaca para crear los métodos de apilamiento, desapilamiento y para revisar el tamaño de la pila.

---
+++ src/polaca/PilaPolaca.java
@@ -0,0 +1,42 @@
+package polaca;
+
+public class PilaPolaca<T> {
+        private Nodo<T> tope;
+        private int size;
+
+        public PilaPolaca() {
+            this.tope = null;
+            this.size = 0;
+        }
+
+        public void apilar(T valor) {
+            Nodo<T> nuevo = new Nodo<>(valor);
+            nuevo.setSiguiente(tope);
+            tope = nuevo;
+            size++;
+        }
+
+        public T desapilar() {
+            if (isEmpty()) {
+                return null;
+            }
+            T valor = tope.getValor();
+            tope = tope.getSiguiente();
+            size--;
+            return valor;
+        }
+
+        public T peek() {
+            if (isEmpty()) {
+                return null;
+            }
+            return tope.getValor();
+        }
+
+        public boolean isEmpty() {
+            return tope == null;
+        }
+        public int getSize() {
+            return size;
+        }
+}
