ca460f533fe28520e52e91f4f4d91643741a8027
SSB-SYS <pbmgamer0@gmail.com>
2026-09-09 22:19:31 -0600

    Añadido la clase Nodo con los getters y setters necesarios para ejecutar el programa.

---
+++ src/polaca/Nodo.java
@@ -0,0 +1,27 @@
+package polaca;
+
+public class Nodo<T> {
+        private T valor;
+        private Nodo<T> siguiente;
+
+        public Nodo(T valor) {
+            this.valor = valor;
+            this.siguiente = null;
+        }
+
+        public T getValor() {
+            return valor;
+        }
+
+        public void setValor(T valor) {
+            this.valor = valor;
+        }
+
+        public Nodo<T> getSiguiente() {
+            return siguiente;
+        }
+
+        public void setSiguiente(Nodo<T> siguiente) {
+            this.siguiente = siguiente;
+        }
+}
