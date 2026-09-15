af9d0c53b7e6de61fe3589ac3a4e58c879953913
SSB-SYS <pbmgamer0@gmail.com>
2026-09-14 23:59:37 -0600

    Correción, el do while es lo que impide que el menú aparezca infinitamente también

--- src/cocina/Main.java
+++ src/cocina/Main.java
@@ -24,66 +24,66 @@ public class Main {
                 continue;
             }
 
-                switch (opcion) {
-                    case 1:
-                        System.out.print("Número de pedido: ");
-                        int num = Integer.parseInt(scanner.nextLine());
-                        System.out.print("Cliente: ");
-                        String cliente = scanner.nextLine();
-                        System.out.print("Platillo: ");
-                        String platillo = scanner.nextLine();
-                        System.out.print("Cantidad: ");
-                        int cant = Integer.parseInt(scanner.nextLine());
-                        System.out.print("Tiempo estimado (mins): ");
-                        int tiempo = Integer.parseInt(scanner.nextLine());
+            switch (opcion) {
+                case 1:
+                    System.out.print("Número de pedido: ");
+                    int num = Integer.parseInt(scanner.nextLine());
+                    System.out.print("Cliente: ");
+                    String cliente = scanner.nextLine();
+                    System.out.print("Platillo: ");
+                    String platillo = scanner.nextLine();
+                    System.out.print("Cantidad: ");
+                    int cant = Integer.parseInt(scanner.nextLine());
+                    System.out.print("Tiempo estimado (mins): ");
+                    int tiempo = Integer.parseInt(scanner.nextLine());
 
-                        cola.enqueue(new Pedido(num, cliente, platillo, cant, tiempo));
-                        break;
+                    cola.enqueue(new Pedido(num, cliente, platillo, cant, tiempo));
+                    break;
 
-                    case 2:
-                        Pedido atendido = cola.dequeue();
-                        if (atendido != null) {
-                            System.out.println("Preparando pedido " + atendido.getNumero());
-                            System.out.println("Cliente: " + atendido.getCliente());
-                            System.out.println("Platillo: " + atendido.getPlatillo());
-                        }
-                        break;
+                case 2:
+                    Pedido atendido = cola.dequeue();
+                    if (atendido != null) {
+                        System.out.println("Preparando pedido " + atendido.getNumero());
+                        System.out.println("Cliente: " + atendido.getCliente());
+                        System.out.println("Platillo: " + atendido.getPlatillo());
+                    }
+                    break;
 
-                    case 3:
-                        Pedido siguiente = cola.peek();
-                        if (siguiente != null) {
-                            System.out.println("Siguiente pedido para entregar: ");
-                            System.out.println(siguiente);
-                        }
-                        break;
+                case 3:
+                    Pedido siguiente = cola.peek();
+                    if (siguiente != null) {
+                        System.out.println("Siguiente pedido para entregar: ");
+                        System.out.println(siguiente);
+                    }
+                    break;
 
-                    case 4:
-                        cola.mostrar();
-                        break;
+                case 4:
+                    cola.mostrar();
+                    break;
 
-                    case 5:
-                        System.out.print("Ingrese el número de pedido a buscar");
-                        int numBuscar = Integer.parseInt(scanner.nextLine());
-                        Pedido encontrado = cola.buscarPedido(numBuscar);
-                        if (encontrado != null) {
-                            System.out.println("Pedido encontrado: " + encontrado);
-                        } else {
-                            System.out.println("El pedido #" + numBuscar + " no existe");
-                        }
-                        break;
+                case 5:
+                    System.out.print("Ingrese el número de pedido a buscar");
+                    int numBuscar = Integer.parseInt(scanner.nextLine());
+                    Pedido encontrado = cola.buscarPedido(numBuscar);
+                    if (encontrado != null) {
+                        System.out.println("Pedido encontrado: " + encontrado);
+                    } else {
+                        System.out.println("El pedido #" + numBuscar + " no existe");
+                    }
+                    break;
 
-                    case 6:
-                        System.out.println("Tiempo total pendiente: " + cola.calcularTiempoTotal() + " mins");
-                        break;
+                case 6:
+                    System.out.println("Tiempo total pendiente: " + cola.calcularTiempoTotal() + " mins");
+                    break;
 
-                    case 7:
-                        System.out.println("Saliendo del sistema");
-                        break;
-                    default:
-                        System.out.println("Opción inválida");
-                }
+                case 7:
+                    System.out.println("Saliendo del sistema");
+                    break;
+                default:
+                    System.out.println("Opción inválida");
             }
-            while (opcion != 7) ;
-            scanner.close();
         }
+        while (opcion != 7);
+        scanner.close();
     }
+}
