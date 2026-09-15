c31bfb7112250e581129a1652b2ae67b57603e36
SSB-SYS <pbmgamer0@gmail.com>
2026-09-14 22:42:15 -0600

    Reacomodadas las llaves de la clase Nodo para mejor organizacion

---
+++ src/cocina/Nodo.java
@@ -0,0 +1,24 @@
+package cocina;
+
+public class Nodo {
+    private Pedido pedido;
+    private Nodo siguiente;
+
+    public Nodo(Pedido pedido) {
+        this.pedido = pedido;
+        this.siguiente = null;
+    }
+
+    public Pedido getPedido() {
+        return pedido;
+    }
+    public void setPedido(Pedido pedido) {
+        this.pedido = pedido;
+    }
+    public Nodo getSiguiente() {
+        return siguiente;
+    }
+    public void setSiguiente(Nodo siguiente) {
+        this.siguiente = siguiente;
+    }
+}
